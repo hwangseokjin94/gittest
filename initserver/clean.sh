55b186295da6e060b24fb62260be222f4dbad948
hwangseokjin94 <vpdnsldk@gmail.com>
2022-05-22 09:11:44 +0900

    initserver

---
+++ initserver/clean.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+cd /opt/initserver/httpd-2.4.53/src/EXTModule/apr-1.7.0/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/EXTModule/apr-1.7.0/test/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/EXTModule/apr-1.7.0/test/internal/ ; make clean ;
+
+cd /opt/initserver/httpd-2.4.53/src/EXTModule/apr-util-1.6.1/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/EXTModule/apr-util-1.6.1/test/ ; make clean ;
+
+cd /opt/initserver/httpd-2.4.53/src/EXTModule/pcre-8.44/ ; make clean ;
+
+cd /opt/initserver/httpd-2.4.53/src/modules/loggers/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/modules/echo/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/modules/database/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/modules/ssl/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/modules/lua/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/modules/filters/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/modules/md/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/modules/cache/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/modules/aaa/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/modules/http2/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/modules/ldap/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/modules/http/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/modules/mappers/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/modules/generators/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/modules/core/test/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/modules/core/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/modules/debugging/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/modules/arch/win32/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/modules/arch/unix/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/modules/dav/fs/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/modules/dav/lock/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/modules/dav/main/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/modules/experimental/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/modules/metadata/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/modules/session/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/modules/proxy/balancers/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/modules/proxy/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/modules/slotmem/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/modules/examples/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/modules/cluster/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/modules/test/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/modules/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/os/unix/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/os/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/server/mpm/worker/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/server/mpm/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/server/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/srclib/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/support/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/test/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/EXTModule/tomcat-connectors-1.2.46-src/native/apache-2.0/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/EXTModule/tomcat-connectors-1.2.46-src/native/common/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/EXTModule/tomcat-connectors-1.2.46-src/native/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/EXTModule/apr-1.7.0.bak/test/internal/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/EXTModule/apr-1.7.0.bak/test/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/EXTModule/apr-1.7.0.bak/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/EXTModule/apr-1.7.0/test/internal/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/EXTModule/apr-1.7.0/test/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/EXTModule/apr-1.7.0/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/EXTModule/apr-util-1.6.1.bak/test/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/EXTModule/apr-util-1.6.1.bak/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/EXTModule/apr-util-1.6.1/test/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/EXTModule/apr-util-1.6.1/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/EXTModule/pcre-8.44.bak/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/EXTModule/pcre-8.44/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/EXTModule/tomcat-connectors-1.2.48-src/native/common/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/EXTModule/tomcat-connectors-1.2.48-src/native/apache-2.0/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/EXTModule/tomcat-connectors-1.2.48-src/native/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/ ; make clean ;
+
+cd /opt/initserver/httpd-2.4.53/src/EXTModule/tomcat-connectors-1.2.48-src/native/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/EXTModule/tomcat-connectors-1.2.48-src/native/common/ ; make clean ;
+cd /opt/initserver/httpd-2.4.53/src/EXTModule/tomcat-connectors-1.2.48-src/native/apache-2.0/ ; make clean ;
+
+
+
